9a5d133670441ea380da7e391bfd80ecaf5dec07
faidakulimushi <faidakulimushi431@gmail.com>
2026-08-17 15:51:16 +0300

    Exercise 7: scrollable app bar

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,37 +1,41 @@
-import { View, StyleSheet, Pressable } from 'react-native';
-import Constants from 'expo-constants';
-import { Link } from 'react-router-native';
-import Text from './Text';
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#24292e',
-    flexDirection: 'row',
-    padding: 15,
-  },
-  tab: {
-    marginRight: 20,
-  },
-  text: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
-});
+import { StyleSheet, View, ScrollView, Text } from "react-native";
+import { Link } from "react-router-native";
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Link to="/" component={Pressable} style={styles.tab}>
-        <Text style={styles.text}>Repositories</Text>
-      </Link>
+      <ScrollView
+        horizontal
+        contentContainerStyle={styles.tabs}
+        showsHorizontalScrollIndicator={false}
+      >
+        <Link style={styles.tab} to="/">
+          <Text style={styles.text}>Repositories</Text>
+        </Link>
 
-      <Link to="/signin" component={Pressable} style={styles.tab}>
-        <Text style={styles.text}>Sign in</Text>
-      </Link>
+        <Link style={styles.tab} to="/signin">
+          <Text style={styles.text}>Sign in</Text>
+        </Link>
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#24292e",
+  },
+  tabs: {
+    flexDirection: "row",
+  },
+  tab: {
+    padding: 15,
+  },
+  text: {
+    color: "#ffffff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
 
 export default AppBar;
