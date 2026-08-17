3ecc47a87840cbf30ae7145448b864114ad5fbcf
faidakulimushi <faidakulimushi431@gmail.com>
2026-08-17 15:31:13 +0300

    Add status bar and routing

--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -1,11 +1,11 @@
-import { View, StyleSheet } from 'react-native';
-import AppBar from './AppBar';
+import { StyleSheet, View } from 'react-native';
+import { Route, Routes, Navigate } from 'react-router-native';
 import RepositoryList from './RepositoryList';
+import AppBar from './AppBar';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e1e4e8',
   },
 });
 
@@ -13,7 +13,11 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+
+      <Routes>
+        <Route path="/" element={<RepositoryList />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </View>
   );
 };
